8cabc1aa2dbb529eaaa938502a1a471d63c6e1bc
marcos vilela <marcospaulo.vilelasantos@gmail.com>
2022-10-07 19:31:15 -0300

    feat: remove info from storage when tab closed

--- background.js
+++ background.js
@@ -3,17 +3,8 @@ import { getLocalState } from './state.js'
 chrome.tabs.onActivated.addListener((async ({tabId, windowsId}) => {
   const { speed, pId } = await getLocalState(tabId);
   chrome.action.setBadgeText({text: `${speed}x`});
-
-  // const foo = await fetchAll()
-  // chrome.storage.sync.remove(Object.keys(foo));
-  // console.log('removing', Object.keys(foo))
 }))
 
-
-async function fetchAll() {
-  return new Promise((resolve, reject) => {
-    chrome.storage.sync.get(null, async (data) => {
-      resolve(data)
-    });
-  })
-}
+chrome.tabs.onRemoved.addListener(async (tabId) => {
+  chrome.storage.sync.remove([String(tabId)]);
+})
